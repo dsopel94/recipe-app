443196093e273639d69d00d1667e4f4f98c37be9
Damian Sopel <dsopel0@gmail.com>
2017-09-24 21:38:20 -0500

    very crude initial work

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="header">
+          <h1>Recipe Creator</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="intro">
+          Find out what you can create with the food in your fridge! Just type in ingredient names below 
+          and recipe suggestions will pop up below
         </p>
+        <div className="input-box">
+        <input className="ingredient-name" id="ingredient-name" />
+        </div>
       </div>
     );
   }
---
+++ src/ingredients.js
@@ -0,0 +1,8 @@
+class Ingredients extends React.Component {
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			ingredients: []
+		}
+	}
+}
